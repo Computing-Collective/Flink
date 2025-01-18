ec003985e79aa22a47429739f962511e99676c66
matthewchow03@gmail.com <matthewchow03@gmail.com>
2025-01-18 13:17:22 -0800

    add protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,26 @@
-// import { Textarea } from "@/components/ui/textarea";
-// import { Button } from "@/components/ui/button";
-// import {
-//   NavigationMenu,
-//   NavigationMenuContent,
-//   NavigationMenuIndicator,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuList,
-//   NavigationMenuTrigger,
-//   NavigationMenuViewport,
-//   navigationMenuTriggerStyle,
-// } from "@/components/ui/navigation-menu";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "@/pages/Login";
 import Generate from "@/pages/Generate";
+import AuthRoute from "@/components/AuthRoute";
 
 function App() {
+  const isAuthenticated = false;
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/generate" element={<Generate />} />
+          <Route
+            path="/generate"
+            element={
+              <AuthRoute
+                isAuthenticated={isAuthenticated}
+                element={<Generate />}
+              />
+            }
+          />
         </Routes>
       </Router>
       );

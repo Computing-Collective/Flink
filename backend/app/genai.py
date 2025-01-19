60d180ccfa0c1178ef425fd9381f2a371393723c
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-18 21:33:19 -0800

    chore(backend/genai): run formatter

--- backend/app/genai.py
+++ backend/app/genai.py
@@ -13,6 +13,7 @@ def video_transcript(video_id: str) -> str:
     sorted_transcript = sorted(raw_transcript, key=lambda x: x["start"])
     return " ".join([x["text"] for x in sorted_transcript])
 
+
 def generate_website_text(url: str, name: str | None, product_name: str) -> str:
     match = re.match(r".*youtube.com/watch\?v=(.*)", url)
     if match is None:
@@ -29,7 +30,10 @@ def generate_website_text(url: str, name: str | None, product_name: str) -> str:
                 "role": "developer",
                 "content": "You will be given a text transcript from an online influencer's content such as a video transcript or blog post. Parse the transcript given and generate a short (under 10 words) summary of why their viewers should buy the product, incorporating some of the themes or content from the transcript. You are personalizing the product page for the influencer's audience to make them more inclined to purchase the given product. Some examples are 'Recommended by Linus Tech Tips for best bass performance' or 'Great for gaming and streaming'. You should use the influencer name in the summary if provided. Do not include quotes around your response.",
             },
-            {"role": "developer", "content": "The influencer name is (ignore if \"NONE\"): " + name},
+            {
+                "role": "developer",
+                "content": 'The influencer name is (ignore if "NONE"): ' + name,
+            },
             {"role": "developer", "content": "The product is: " + product_name},
             {"role": "user", "content": "Here is the transcript: " + transcript_text},
         ],

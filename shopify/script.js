6e612ee4cfc61f621037b2e63cf3921847dc11e4
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-18 20:10:21 -0800

    feat(website-script): improve personlization script

--- shopify/script.js
+++ shopify/script.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  console.log("Personalize script loaded");
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
+  if (!code) {
+    return
+  }
+  console.log("Code:", code);
 
   try {
-    const response = await fetch(`http://localhost:8000/link/${code}`);
+    const response = await fetch(`http://localhost:8000/link/${code}/decode`);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      console.error("HTTP error:", response.status);
     }
     const json = await response.json();
-    const summary = json.summary;
+    const summary = json.website_text;
 
     const bannerHeading = document.querySelector(".banner__heading");
     if (bannerHeading) {

377aff59d000e329095dd1dcdc14d4849d68e081
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-18 17:08:34 -0800

    feat(backend): database interaction

---
+++ backend/app/database.py
@@ -0,0 +1,51 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine, select
+
+from app.models import *
+from app.security import get_password_hash, verify_password
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+def create_user(*, session: Session, user: UserRegister) -> User:
+    db_user = User.model_validate(
+        user, update={"hashed_password": get_password_hash(user.password)}
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+def get_user_by_email(*, session: Session, email: str) -> User | None:
+    statement = select(User).where(User.email == email)
+    session_user = session.exec(statement).first()
+    return session_user
+
+
+def authenticate(*, session: Session, email: str, password: str) -> User | None:
+    db_user = get_user_by_email(session=session, email=email)
+    if not db_user:
+        return None
+    if not verify_password(password, db_user.hashed_password):
+        return None
+    return db_user

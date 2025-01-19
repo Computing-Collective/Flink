44218a7a35ca60f70e55ab193d1e71a73fb12f89
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-01-18 21:28:26 -0800

    feat(backend/links): error handling and delete link endpoint

--- backend/app/routes/links.py
+++ backend/app/routes/links.py
@@ -1,6 +1,6 @@
 import urllib.parse as urlparse
 from urllib.parse import urlencode
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlmodel import select
 
@@ -10,10 +10,13 @@ from app.models import *
 router = APIRouter(tags=["links"])
 
 
-@router.post("/create_link")
+@router.post("/create_link", response_model=LinkPublic)
 def create_link(link: LinkCreate, session: db.SessionDep):
-    db_link = db.create_link(session=session, link=link)
-    return LinkPublic.model_validate(db_link)
+    try:
+        db_link = db.create_link(session=session, link=link)
+        return LinkPublic.model_validate(db_link)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid link data")
 
 
 @router.get("/links", response_model=LinksPublic)
@@ -40,7 +43,20 @@ def get_link(code: str, session: db.SessionDep):
     url_parts[4] = urlencode(query)
     return RedirectResponse(urlparse.urlunparse(url_parts))
 
+
+@router.delete("/link/{code}")
+def delete_link(code: str, session: db.SessionDep):
+    link = session.exec(select(Link).where(Link.code == code)).first()
+    if not link:
+        raise HTTPException(status_code=404, detail="Link not found")
+    session.delete(link)
+    session.commit()
+    return {"message": "Link deleted"}
+
+
 @router.get("/link/{code}/decode", response_model=LinkDecode)
 def get_link_info(code: str, session: db.SessionDep):
     link = session.exec(select(Link).where(Link.code == code)).first()
+    if not link:
+        raise HTTPException(status_code=404, detail="Link not found")
     return LinkDecode(website_text=link.website_text)
